6c15ae93e7b678ba4c35af1a5c3cc45b374cfead
Snigdha Anzuman <114046009+Anzuman06@users.noreply.github.com>
2023-04-17 08:44:31 +0600

    Add files via upload

--- Project5/index.js
+++ Project5/index.js
@@ -1,8 +1,31 @@
  // API for get requests
+ const agent = window.Agent;
  const GETCallButton = document.getElementById('GET');
- async function getData(url = "https://jsonplaceholder.typicode.com/posts") {
- const response = await fetch(url,{method: "GET"});
- const jsonData = await response.json();
-console.log(jsonData);
- }
+ const POSTCallButton = document.getElementById('POST');
+ const init = async () => {
+  async function getData(url = "https://jsonplaceholder.typicode.com/posts",data={}) {
+  const res = await fetch(url);
+  const jsonData = await res.json();
+  device.textContent = JSON.stringify(jsonData);
+  console.log('Got onResponse', JSON.stringify(jsonData));
+  }
+ async function postData(url = "https://jsonplaceholder.typicode.com/posts",data={}) {
+    const response = await fetch(url,{
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'testing',
+    body: 'the query is good',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+});
+ const jsonD = await response.json();
+ device.textContent = JSON.stringify(jsonD);
+ console.log('Got onResponse', JSON.stringify(jsonD));
+}
  GETCallButton.onclick = () => getData();
+ POSTCallButton.onclick = () => postData();
+};
+init();
